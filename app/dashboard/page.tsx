daa4594f8ed6df586df64526c25a133b75c09fc1
happycode2708 <happycode2708@gmail.com>
2023-11-25 18:44:07 +0700

    feat(db latest invoices): add latest invoices list

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 // import { Card } from '@/app/ui/dashboard/cards';
 // import RevenueChart from '@/app/ui/dashboard/revenue-chart';
-import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+// import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import {
   fetchRevenue,
@@ -24,6 +24,7 @@ const Dashboard_page = async () => {
 
   return (
     <main>
+      labe
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1>
@@ -55,10 +56,10 @@ const Dashboard_page = async () => {
         <DbRevenueChart revenue={revenue} />
         <DbLatestInvoices latestInvoices={latestInvoices} />
       </div>
-      <div className='mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8'>
-        {/* <RevenueChart revenue={revenue} /> */}
+      {/* <div className='mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8'>
+        <RevenueChart revenue={revenue} />
         <LatestInvoices latestInvoices={latestInvoices} />
-      </div>
+      </div> */}
     </main>
   );
 };
---
+++ app/ui/dashboard/db-latest-invoices.tsx
@@ -0,0 +1,48 @@
+import { LatestInvoice } from '@/app/lib/definitions';
+import Image from 'next/image';
+import { lusitana } from '@/app/ui/fonts';
+
+const DbLatestInvoinces = (props: { latestInvoices: LatestInvoice[] }) => {
+  const { latestInvoices } = props;
+
+  return (
+    <div className='md:col-span-4 flex flex-col'>
+      <h2 className={`test-xl md:text-2xl ${lusitana.className}`}>
+        Latest Invoices
+      </h2>
+      <div className='bg-gray-50 rounded-xl p-5 flex-grow'>
+        <div className='bg-white h-full divide-y-2 divide-gray-100 px-3'>
+          {latestInvoices.map((invoiceItem) => (
+            <>
+              <InvoiceItem key={invoiceItem.id} invoiceData={invoiceItem} />
+            </>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const InvoiceItem = ({ invoiceData }: { invoiceData: LatestInvoice }) => {
+  const { image_url, name, email, amount } = invoiceData;
+
+  return (
+    <div className='flex flex-row items-center py-5 px-3'>
+      <Image
+        className='rounded-full mr-3'
+        alt='invoice avatar'
+        src={image_url}
+        width={35}
+        height={35}
+      />
+      <div className='overflow-hidden'>
+        <p className='truncate text-base font-semibold'>{name}</p>
+        <p className='text-gray-500 text-sm'> {email} </p>
+      </div>
+      <div className='flex-grow'></div>
+      <p className={`${lusitana.className} pl-5`}>{amount}</p>
+    </div>
+  );
+};
+
+export default DbLatestInvoinces;
