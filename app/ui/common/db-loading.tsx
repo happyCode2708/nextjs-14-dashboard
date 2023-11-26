4a398e6a2042c31ce9d5878b06e3fc1c9d0b897f
happycode2708 <happycode2708@gmail.com>
2023-11-26 18:25:19 +0700

    feat(common loading): add common loading indicator to the app

---
+++ app/ui/common/db-loading.tsx
@@ -0,0 +1,7 @@
+export const DbLoading = () => {
+  return (
+    <div
+      className={`animate-spin border-[7px] w-[35px] h-[35px] border-[#f3f3f3] border-t-blue-200 m-auto rounded-full`}
+    ></div>
+  );
+};
