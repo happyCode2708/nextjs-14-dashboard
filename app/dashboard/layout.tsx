48afa23dddef0801e65d4a254b3a872a22f1f321
happycode2708 <happycode2708@gmail.com>
2023-11-26 18:27:48 +0700

    fix(layout): fix should scroll inside main content and let it expand all to right

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,23 +2,11 @@ import SideNav from '@/app/ui/dashboard/sidenav';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div
-      // className='flex h-screen flex-col md:flex-row md:overflow-hidden'
-      // className='flex h-screen md:flex-row flex-col'
-      className='flex min-h-screen md:flex-row flex-col p-[3px]'
-    >
-      <div
-        // className='w-full flex-none md:w-64'
-        className='w-full md:w-64 md:mr-1'
-      >
+    <div className='flex h-screen md:flex-row flex-col p-[3px]'>
+      <div className='w-full md:w-64 md:mr-1'>
         <SideNav />
       </div>
-      <div
-        // className='flex-grow p-6 md:overflow-y-auto md:p-12'
-        className='grow'
-      >
-        {children}
-      </div>
+      <div className='h-screen flex-1 overflow-auto'>{children}</div>
     </div>
   );
 }
